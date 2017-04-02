4cdf103a0829c31461ecb40bf0bb261271b627e5
Swag7User <ccodegeass@cock.li>
2017-04-02 15:38:29 +0200

    p2p bootstrapping

---
+++ src/main/java/ch/uzh/helper/P2POverlay.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.uzh.helper;
+
+import javafx.util.Pair;
+import net.tomp2p.dht.*;
+import net.tomp2p.futures.BaseFuture;
+import net.tomp2p.futures.BaseFutureAdapter;
+import net.tomp2p.futures.FutureBootstrap;
+import net.tomp2p.futures.FutureDirect;
+import net.tomp2p.p2p.PeerBuilder;
+import net.tomp2p.p2p.Peer;
+import net.tomp2p.peers.Number160;
+import net.tomp2p.peers.PeerAddress;
+import net.tomp2p.rpc.ObjectDataReply;
+import net.tomp2p.storage.Data;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Random;
+
+/**
+ * Basically a wrapper for TomP2P for the needs of this application.
+ * @author Sebastian
+ */
+public class P2POverlay {
+
+    private Peer peer;
+    private PeerDHT peerDHT;
+    private static Random rnd = new Random();
+
+    public PeerAddress getPeerAddress() {
+        return peer.peerAddress();
+    }
+
+    public boolean put(String key, Object value) {
+        Data data;
+        try {
+            data = new Data(value);
+        } catch (IOException ex) {
+            return false;
+        }
+
+        FuturePut futurePut = peerDHT.put(Number160.createHash(key)).data(data).start().awaitUninterruptibly();
+
+        return futurePut.isSuccess();
+    }
+
+    public Object getBlocking(String key) {
+        FutureGet futureGet = peerDHT.get(Number160.createHash(key)).start().awaitUninterruptibly();
+
+        if (futureGet.isSuccess()) {
+            try {
+                return futureGet.data().object();
+            } catch (ClassNotFoundException | IOException ex) {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public boolean sendBlocking(PeerAddress recipient, Object o) {
+        FutureDirect futureDirect = peer.sendDirect(recipient)
+                .object(o).start().awaitUninterruptibly();
+
+        return futureDirect.isSuccess();
+    }
+
+    public void sendNonBlocking(PeerAddress recipient, Object o, boolean forceUDP) {
+        FutureDirect futureDirect = peer.sendDirect(recipient)
+                .object(o).forceTCP(forceUDP).start();
+    }
+
+    public void getNonBLocking(String key, BaseFutureAdapter<FutureGet> baseFutureAdapter) {
+        FutureGet futureGet = peerDHT.get(Number160.createHash(key)).start();
+        futureGet.addListener(baseFutureAdapter);
+    }
+
+    public Pair<Boolean, String> bootstrap(String bootstrapIP) {
+        int port = 4001;
+
+        // Create TomP2P peer
+        boolean peerCreated = false;
+        do {
+            try {
+                // Create new peer
+                peer = new PeerBuilder(new Number160(rnd)).ports(port++).start();
+                peerDHT = new PeerBuilderDHT(peer).start();
+                peerCreated = true;
+            } catch (IOException ex) {
+                System.out.println("Port already in use. " + ex.getMessage());
+            }
+        } while (!peerCreated && port < 4010);
+
+        if (!peerCreated) {
+            return new Pair<>(false, "Could not find any unused port");
+        }
+
+        try {
+            peer.discover().inetAddress(InetAddress.getByName(bootstrapIP)).ports(4001).start().awaitUninterruptibly();
+            FutureBootstrap futureBootstrap = peer.bootstrap().inetAddress(InetAddress.getByName(bootstrapIP)).ports(4001).start();
+            futureBootstrap.awaitUninterruptibly();
+            if (futureBootstrap.isSuccess()) {
+                return new Pair<>(true, "Bootstrap successful");
+            } else {
+                return new Pair<>(false, "Could not bootstrap to well known peer");
+            }
+        } catch (UnknownHostException ex) {
+            return new Pair<>(false, "Unknown bootstrap host. (UnknownHostException)");
+        }
+
+    }
+
+    public void setObjectDataReply(ObjectDataReply replyHandler) {
+        peer.objectDataReply(replyHandler);
+    }
+
+    public void shutdown() {
+        if (peer != null) {
+            BaseFuture shutdownBuilder = peer.shutdown();
+            shutdownBuilder.awaitUninterruptibly();
+            peer = null;
+        }
+    }
+}
--- src/main/java/ch/uzh/model/LoginWindow.java
+++ src/main/java/ch/uzh/model/LoginWindow.java
@@ -1,6 +1,7 @@
 package ch.uzh.model;
 
 import ch.uzh.controller.LoginWindowController;
+import ch.uzh.helper.P2POverlay;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,14 +9,35 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 
 public class LoginWindow extends Application {
     private Stage stage;
+    private P2POverlay p2p;
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
+
+        // Get parameters
+        String bootstrapIP = getParameters().getNamed().get("bootstrap");
+        bootstrapIP = "127.0.0.1";
+
+        p2p = new P2POverlay();
+
+        // Try to bootstrap
+        Pair<Boolean, String> result = p2p.bootstrap(bootstrapIP);
+        if (result.getKey() == false) {
+            System.err.println("Aw shit, didn't work\n");
+        } else{
+            System.err.println("it's AWRIGHT\n");
+        }
+
+        System.out.println("Bootstrapped to: " + bootstrapIP
+                + "My IP: " + p2p.getPeerAddress().inetAddress().getHostAddress());
+
+
         System.err.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.err.println("FXML resource: " + getClass().getResource("ch/uzh/model/LoginWindow.fxml"));
         System.err.println("FXML resource: " + getClass().getResource("LoginWindow.fxml"));
@@ -32,6 +54,20 @@ public class LoginWindow extends Application {
         stage.setTitle("Misaka - Login");
         stage.setScene(scene);
         stage.show();
+    }
+
+    /**
+     * Application shutdown *
+     */
+    @Override
+    public void stop() {
+        shutdown();
+    }
+
+    public void shutdown() {
+
+        // Shutdown Tom P2P stuff
+        p2p.shutdown();
     }
 
     public Stage getStage() {
