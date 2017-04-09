0f1e02e1f29f307bc0ab081f7403573172ad4268
Swag7User <ccodegeass@cock.li>
2017-04-09 16:40:13 +0200

    friend p2p adding

--- src/main/java/ch/uzh/controller/MenuOverlayController.java
+++ src/main/java/ch/uzh/controller/MenuOverlayController.java
@@ -58,6 +58,7 @@ public class MenuOverlayController {
                     }
 
                     userID = null;
+                    addFriendField.clear();
                 }
             }
         });
